6db6b4fe41e85056408b8fd15f8b21aa3563680c
Ayrton G Guerra <ayrton.guerra@cliqx.com.br>
2023-08-09 18:18:50 -0300

    Service created and add async methods

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,4 +1,3 @@
-global using dotnet_rpg.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +10,27 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController: ControllerBase
     {
-        private static Character knight = new Character();
+        private readonly ICharacterService _characterService;
 
-        [HttpGet]
-        public ActionResult<Character> Get() {
-            return Ok(knight);
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<List<Character>>> Get() {
+            return Ok(await _characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Character>> GetSingle(int id) {
+            return Ok(await _characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Character>>> AddCharacter(Character newCharacter)
+        {
+            return Ok(await _characterService.AddCharacter(newCharacter));
         }
     }
 }
